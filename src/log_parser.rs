61fd9553b403da84fdd4ce18f1f91ab7d5cefea5
jmangefrida <jmange@gmail.com>
2023-07-07 20:56:18 -0400

    parser work

--- src/log_parser.rs
+++ src/log_parser.rs
@@ -1,7 +1,7 @@
 use crate::log_event;
 use std::collections::HashMap;
 use serde_json::{Result, Value};
-
+use std::str;
 
 
 pub fn parse_syslog(event: log_event::LogEvent) -> log_event::AnalyzedEvent {
@@ -12,7 +12,13 @@ pub fn parse_syslog(event: log_event::LogEvent) -> log_event::AnalyzedEvent {
 }
 
 fn extract_pri(event: &mut log_event::AnalyzedEvent) {
+    if event.event.msg.starts_with("<") {
+        let end = event.event.msg.find(">");
+        
+    }
+
     event.data.get_mut("pri").get_or_insert(&mut "value".to_string());
+
 }
 
 
